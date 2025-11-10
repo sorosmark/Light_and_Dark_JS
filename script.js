5c6d211610cca7956f42433a5d57d908f64f727f
sorosmark <soros.mark@diak.szbi-pg.hu>
2025-11-10 10:58:20 +0100

    navbar

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function switchTheme() {
   document.body.classList.add('bg-dark')
+  
+  const navbar = document.getElementById('navbar')
+
+  navbar.classList.remove('navbar-light' , 'bg-light')
+  navbar.classList.add('navbar-dark', 'bg-dark')
 }
