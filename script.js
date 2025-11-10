e98366291451bc28258333eb4e432021154e639f
sorosmark <soros.mark@diak.szbi-pg.hu>
2025-11-10 11:06:30 +0100

    3.feladat

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ function switchTheme() {
   document.body.classList.add('bg-dark')
   
   const navbar = document.getElementById('navbar')
+  const cards = document.querySelectorAll('.card')
 
-  navbar.classList.remove('navbar-light' , 'bg-light')
+  navbar.classList.remove('navbar-light', 'bg-light')
   navbar.classList.add('navbar-dark', 'bg-dark')
+
+  cards.forEach(card => {
+    card.classList.add('text-white', 'bg-dark', 'border-light')
+  })
 }
