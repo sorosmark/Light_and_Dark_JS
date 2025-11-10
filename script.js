12acd288de7860bc25c48cc39c3cfccaeeea9204
sorosmark <soros.mark@diak.szbi-pg.hu>
2025-11-10 11:24:01 +0100

    4.feladat

--- script.js
+++ script.js
@@ -3,11 +3,16 @@ function switchTheme() {
   
   const navbar = document.getElementById('navbar')
   const cards = document.querySelectorAll('.card')
+  const tables = document.querySelectorAll('.table')
 
   navbar.classList.remove('navbar-light', 'bg-light')
   navbar.classList.add('navbar-dark', 'bg-dark')
 
   cards.forEach(card => {
     card.classList.add('text-white', 'bg-dark', 'border-light')
+  })
+
+  tables.forEach(table => {
+    table.classList.add('table-dark')
   })
 }
