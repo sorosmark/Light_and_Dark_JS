e8344c546c6bf1460d8c9fccb7456407a53e288d
sorosmark <soros.mark@diak.szbi-pg.hu>
2025-11-10 12:38:12 +0100

    vege

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let theme = false;
+
 function switchTheme() {
   document.body.classList.add('bg-dark')
   
@@ -7,26 +9,37 @@ function switchTheme() {
   const cardcolumn = document.querySelectorAll('.column')
   const footertext = document.querySelector('.navbar-text')
   const h1title = document.getElementById('h1_title')
+  const button = document.getElementById('themeSwitch')
 
-  navbar.classList.remove('navbar-light', 'bg-light')
-  navbar.classList.add('navbar-dark', 'bg-dark')
 
-  cards.forEach(card => {
-    card.classList.add('text-white', 'bg-dark', 'border-light')
-  })
-
-  tables.forEach(table => {
-    table.classList.add('table-dark')
-  })
-
-  cardcolumn.forEach(card => {
-    card.classList.remove('col')
-    card.classList.add('col-12')
-  })
-
-  footertext.style.paddingLeft = '1700px'
-  footertext.style.display = 'block'
-  footertext.classList.add('text-white')
-
+  if (theme === false) {
+    navbar.classList.remove('navbar-light', 'bg-light')
+    navbar.classList.add('navbar-dark', 'bg-dark')
+    cards.forEach(card => {
+      card.classList.add('text-white', 'bg-dark', 'border-light')
+    })
   
+    tables.forEach(table => {
+      table.classList.add('table-dark')
+    })
+  
+    cardcolumn.forEach(card => {
+      card.classList.remove('col')
+      card.classList.add('col-12')
+    })
+  
+    footertext.style.paddingLeft = '1700px'
+    footertext.style.display = 'block'
+    footertext.classList.add('text-white')
+  
+    h1title.innerHTML = "This is the dark theme"
+    h1title.classList.add("text-white")
+  
+    button.innerHTML = "Activate Light Theme"   
+  }
+  else{
+    navbar.classList.remove('navbar-dark', 'bg-dark')
+    navbar.classList.add('navbar-light', 'bg-light')
+  }
+  theme = !theme
 }
